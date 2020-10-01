15cd46cddb64740678f5663ea61d124c60d98c33
John <jpending@gmail.com>
2020-10-01 17:44:43 -0400

    barebones

---
+++ modules/app/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const helmet=require('helmet');
+const express=require('express')
+const cors=require('cors')
+const morgan=require('morgan');
+
+const {NODE_ENV, CLIENT_ORIGIN}=require('./config');
+
+const app=express();
+app.use(morgan('tiny'));
+app.use(helmet());
+app.use(cors({
+  origin: CLIENT_ORIGIN
+}));
+
+app.use('/people', require('../people/people.router'))
+app.use('/pets', require('../pets/pets.router'))
+
+module.exports=app
